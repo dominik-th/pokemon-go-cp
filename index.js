be678036ab5567f42c36f4778df3e05c86c0c26b
dominik-th <dominik-th@users.noreply.github.com>
2017-11-24 19:47:45 +0100

    Add Pokémon Go combat power formula

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 exports.calculate = (pokemon, atk, def, hp) => {
   return 1;
 }
+
+combatPowerForumla = (atk, def, hp, multiplier) => {
+  const normalizedAtk = atk;
+  const normalizedDef = Math.pow(def, .5);
+  const normalizedHp = Math.pow(hp, .5);
+  const normalizedMultiplier = Math.pow(multiplier, 2);
+  const cp = Math.floor((normalizedAtk * normalizedDef * normalizedHp * normalizedMultiplier) / 10);
+  return Math.max(cp, 10);
+}
